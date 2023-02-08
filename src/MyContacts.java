c326faa868cf2bec78b980124323f61bb4a670fc
Davidmartcab <davidmartcab@gmail.com>
2023-02-08 12:09:48 +0100

    Added Goyox

--- src/MyContacts.java
+++ src/MyContacts.java
@@ -6,6 +6,7 @@ public class MyContacts {
         return new HashMap<>(){{
             put("192.168.13.28", new IpPort(9090, "Mario"));
             put("192.168.13.22", new IpPort(9090, "Asier"));
+            put("192.168.13.21", new IpPort(9090, "Goyo"));
             put("192.168.13.37", new IpPort(9090, "David"));
         }};
     }
