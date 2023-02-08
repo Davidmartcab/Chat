fc929c47dd18225aa281a9cb49406c1bc1a97d48
Davidmartcab <davidmartcab@gmail.com>
2023-02-08 11:38:07 +0100

    Set Time Out

--- src/App.java
+++ src/App.java
@@ -26,7 +26,7 @@ public class App {
                 }
             }
         } catch (Exception e) {
-            System.out.println("C. Error: " + e.getMessage());
+            System.out.println("A. Error: " + e.getMessage());
         }
     }
 }
--- src/Client.java
+++ src/Client.java
@@ -17,15 +17,17 @@ public class Client extends Thread{
         try {
             sender(mensaje, ip, port);
         } catch (Exception e) {
-            System.out.println("C. Error: " + e.getMessage());
+            System.out.println("C. Error: El mensaje: '" + mensaje + "', no ha llegado al receptor: " + ip + ".");
         }
     }
 
     public void sender(String mensaje, String ip, int port) throws UnknownHostException, IOException{
         // Realiza la conexión
-        Socket socket = new Socket(ip, port);
+        Socket socket = new Socket();
+        // Esto siver para realizar la conexión y configurar un timeOut máximo de 5 segundos
+        socket.connect(new InetSocketAddress(ip, port), 5000);
         // System.out.println("C. Conectado al servidor");
-    
+        
         // PrintWriter es el encargado de enviar un mensaje al servidor
         PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
         // BufferedReader es el encargado de recibir la respuesta del servidor tras enviar el mensaje
