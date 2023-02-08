77ce6fd8599f04e38814f6328f292c34bcfcd9cc
Davidmartcab <davidmartcab@gmail.com>
2023-02-08 11:13:37 +0100

    a

--- src/Server.java
+++ src/Server.java
@@ -8,8 +8,7 @@ public class Server{
 
 
     private final static HashMap<String, IpPort> map = new HashMap<>(){{
-        put("192.168.13.28", new IpPort(9090, "Mario"));
-        put("192.168.13.22", new IpPort(9090, "Asier"));
+
         put("192.168.13.37", new IpPort(9090, "David"));
     }};
 
