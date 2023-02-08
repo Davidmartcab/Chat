83fecd18634ec3d27c3cb5fec7fdcfdc6f51af08
Davidmartcab <davidmartcab@gmail.com>
2023-02-08 10:43:07 +0100

    Lista de contactos

--- src/App.java
+++ src/App.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class App {
 // Hay que ejecutar el Server.java y el App.java, en ese orden.
 // El App.java es el cliente, y el Server.java es el servidor.
+// Ir añadiendo aquí ti IP para añadirte a la lista de contactos.
     private final static HashMap<String, IpPort> map = new HashMap<>(){{
         put("192.168.13.28", new IpPort(9090, "Mario"));
         put("192.168.13.22", new IpPort(9090, "Asier"));
