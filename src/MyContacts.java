6fdf943f85dc5f9a954b99c9cb95003d0ea90c62
Davidmartcab <davidmartcab@gmail.com>
2023-02-08 11:58:05 +0100

    Change

--- src/MyContacts.java
+++ src/MyContacts.java
@@ -1,13 +1,12 @@
 import java.util.HashMap;
 
 public class MyContacts {
-    private static final HashMap<String, IpPort> map = new HashMap<>(){{
-        put("192.168.13.28", new IpPort(9090, "Mario"));
-        put("192.168.13.22", new IpPort(9090, "Asier"));
-        put("192.168.13.37", new IpPort(9090, "David"));
-    }};
 
     public static HashMap<String, IpPort> getMap() {
-        return map;
+        return new HashMap<>(){{
+            put("192.168.13.28", new IpPort(9090, "Mario"));
+            put("192.168.13.22", new IpPort(9090, "Asier"));
+            put("192.168.13.37", new IpPort(9090, "David"));
+        }};
     }
 }
